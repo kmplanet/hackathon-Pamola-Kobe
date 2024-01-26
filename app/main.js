111c327f662db6c7fb768da8a4379559a700be26
Kobe Fung <kobefungs@gmail.com>
2024-01-26 16:37:06 +0700

    tested in chrome extension it worked from my end.

--- app/main.js
+++ app/main.js
@@ -1,6 +1,3 @@
-const newQButton = document.getElementById("newQuoteButton");
-newQButton.addEventListener("click", fetchData);
-
 async function fetchData() {
   const res = await fetch("https://api.quotable.io/random");
   const record = await res.json();
