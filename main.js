a8806ae6db068811b0fe47511bf77c8e3d594082
Kobe Fung <kobefungs@gmail.com>
2024-01-26 11:04:54 +0700

    added button click action

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+const newQButton = document.getElementById("newQuoteButton");
+newQButton.addEventListener("click", fetchData);
+
 async function fetchData() {
-    const res=await fetch ("https://api.quotable.io/random");
-    const record  =await res.json();
-    document.getElementById("quote").innerHTML=record.content;
-    document.getElementById("author").innerHTML=record.author;
+  const res = await fetch("https://api.quotable.io/random");
+  const record = await res.json();
+  document.getElementById("quote").innerHTML = record.content;
+  document.getElementById("author").innerHTML = record.author;
 }
 fetchData();
